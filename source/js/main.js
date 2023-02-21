de207dc8763ee07423d5539115171affd89ebb10
Anatoly U <korvet07@yandex.ru>
2023-02-21 09:32:26 +0300

    first commit

---
+++ source/js/main.js
@@ -0,0 +1,56 @@
+import {iosVhFix} from './utils/ios-vh-fix';
+import {initModals} from './modules/modals/init-modals';
+
+const giftsForm = document.querySelector('.gifts-list__form');
+
+// ---------------------------------
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Utils
+  // ---------------------------------
+  iosVhFix();
+  if (giftsForm) {
+    submitForm();
+  }
+
+  // Modules
+  // ---------------------------------
+
+  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
+  // в load следует добавить скрипты, не участвующие в работе первого экрана
+  window.addEventListener('load', () => {
+    initModals();
+  });
+});
+
+function submitForm() {
+  giftsForm.addEventListener('change', (event) => {
+    if (event.target && event.target.closest('.gifts-list__form')) {
+      giftsForm.requestSubmit();
+    }
+  });
+}
+// ---------------------------------
+
+// ❗❗❗ обязательно установите плагины eslint, stylelint, editorconfig в редактор кода.
+
+// привязывайте js не на классы, а на дата атрибуты (data-validate)
+
+// вместо модификаторов .block--active используем утилитарные классы
+// .is-active || .is-open || .is-invalid и прочие (обязателен нейминг в два слова)
+// .select.select--opened ❌ ---> [data-select].is-open ✅
+
+// выносим все в дата атрибуты
+// url до иконок пинов карты, настройки автопрокрутки слайдера, url к json и т.д.
+
+// для адаптивного JS используется matchMedia и addListener
+// const breakpoint = window.matchMedia(`(min-width:1024px)`);
+// const breakpointChecker = () => {
+//   if (breakpoint.matches) {
+//   } else {
+//   }
+// };
+// breakpoint.addListener(breakpointChecker);
+// breakpointChecker();
+
+// используйте .closest(el)
